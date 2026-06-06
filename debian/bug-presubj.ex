f3d2c3e0caf9ddeb07e3615802f4084d5160178e
Fabio Fantoni <fantonifabio@tiscali.it>
2026-06-06 21:32:55 +0200

    debian: partial clean of template files

--- debian/bug-presubj.ex
+++
@@ -1,3 +0,0 @@
-# Installed as usr/share/bug/nscp/presubj in nscp
-# See dh_bugfiles(1) and /usr/share/doc/reportbug/README.developers.gz
-# The contents of this file are displayed to the user by the bug reporting tool.
